b13c4ef34fb13b2af6273f6eb751eca375dc2d93
田昶尧 <tcyhost@buaa.edu.cn>
2021-03-14 20:06:50 +0800

    bug fixed

--- test/test_server.py
+++ test/test_server.py
@@ -3,7 +3,7 @@ import os
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(os.path.split(rootPath)[0])
-from myQueue.utils import *
+from utils import *
 
 class Test_base:
     # 函数级开始

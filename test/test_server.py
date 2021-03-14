0f609d16bad8b30e70fe39190c9cbbc90ff67b52
田昶尧 <tcyhost@buaa.edu.cn>
2021-03-14 20:00:56 +0800

    bug fixed

--- test/test_server.py
+++ test/test_server.py
@@ -1,9 +1,9 @@
-
 from myQueue.utils import *
 import sys
 import os
-sys.path.append(
-    (os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))))
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(os.path.split(rootPath)[0])
 
 
 class Test_base:
